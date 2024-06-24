15c9aaf43ca37c2717d75377c656cee1511294c7
Jardel Costa <jardel.costa@gmail.com>
2024-06-24 16:41:15 -0300

    Entrega do Projeto

--- dio/desafio-dio/index.py
+++ dio/desafio-dio/index.py
@@ -3,25 +3,25 @@ level=0
 nivel=""
 
 nome=str(input("Valente Heroi, diga-me seu nome! Para que a glória de seus feitos seja cantada pelos bardos!:"))
-print(f"Seja bem-vindo ao reino de Aethelmere, Valente Caveleiro!")
+print("Seja bem-vindo ao reino de Aethelmere, Valente Caveleiro!")
 print("")
 level=int(input("Diga-me, Heroi, qual a sua quantidade de experiência em xp? É um novato em busca de glória, ou um veterano de mil batalhas?"))
 
-if (level<1000):
+if (level<=1000):
     nivel="Ferro"
-elif (level>1001 and level<2000):
+elif (level>=1001 and level<=2000):
     nivel="Bronze"
-elif (level>2001 and level<5000):
+elif (level>=2001 and level<=5000):
     nivel="Prata"
-elif (level>5001 and level<7000):
+elif (level>=5001 and level<=7000):
     nivel="Ouro"
-elif (level>7001 and level<8000):
+elif (level>=7001 and level<=8000):
     nivel="Platina"
-elif (level>8001 and level<9000):
+elif (level>=8001 and level<=9000):
     nivel="Ascendente"
-elif (level>9001 and level<10000):
+elif (level>=9001 and level<=10000):
     nivel="Imortal"
-elif (level>10001):
+elif (level>=10001):
     nivel="Radiante"
 else:
     print("Sua inscrição não parece correta, nobre guerreiro! Verifique se declarou a informação corretamente, e tente novamente. A sorte esteja com você!")

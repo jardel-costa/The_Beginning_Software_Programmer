8230fc3cc51d1ac6153fd32c3525a7339cd34e43
Jardel Costa <jardel.costa@gmail.com>
2024-07-08 19:27:04 -0300

    updating

---
+++ javaScript/Seletores-DOM/index.js
@@ -0,0 +1,26 @@
+//console.log("Olá mundo!")
+
+//const titulo = document
+
+//console.log(titulo)
+
+const titulo = document.getElementById("titulo")
+
+//console.log(titulo)
+
+const textos = document.getElementsByClassName("texto-simples");
+
+//console.log(textos)
+
+//console.log(textos[0])
+
+const divh2 = document.querySelector("div h2");
+
+//console.log(divh2);
+
+const textosPorClasse = document.querySelectorAll(".texto-simples");
+
+console.log(textosPorClasse[1]);
+
+
+

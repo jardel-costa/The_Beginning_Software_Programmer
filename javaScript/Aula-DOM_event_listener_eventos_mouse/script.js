4f96115ab5d6f2687a0320f6299113db390b093f
Jardel Costa <jardel.costa@gmail.com>
2024-07-11 20:30:15 -0300

    Update

--- javaScript/Aula-DOM_event_listener_eventos_mouse/script.js
+++ javaScript/Aula-DOM_event_listener_eventos_mouse/script.js
@@ -8,3 +8,40 @@ let subtotalInfo = {
 
 quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
 valorSubtotal.innerText = subtotalInfo.valor;
+
+// Seleção botões
+
+// Botão Add
+let btnAddProd01 = document.querySelector("#btn-adicionar-produto-01")
+console.log(btnAddProd01)
+
+// Botão Sub
+let btnSubProd01 = document.querySelector("#btn-subtrair-produto-01")
+console.log(btnSubProd01)
+
+// Seleção Qtde "input"
+let qtdeProd01 = document.querySelector("#quantidade-produto-01")
+console.log(qtdeProd01)
+
+// Funções:
+
+//Add
+function adicionarProduto() {
+  qtdeProd01.value = Number(qtdeProd01.value) + 1;
+} 
+
+//Sub
+function subtrairProduto() {
+  if (qtdeProd01.value > 0) {
+    qtdeProd01.value = Number(qtdeProd01.value) - 1;
+  }
+  
+} 
+
+// Eventos
+
+//Add
+btnAddProd01.addEventListener("click", adicionarProduto)
+
+//Sub
+btnSubProd01.addEventListener("click", subtrairProduto)

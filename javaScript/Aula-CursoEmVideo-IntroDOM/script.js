c9d09b3d48f8dd1deaca462c8592bf872e637574
Jardel Costa <jardel.costa@gmail.com>
2024-07-17 21:16:46 -0300

    Iniciando estudos DOM - Curso em Video

---
+++ javaScript/Aula-CursoEmVideo-IntroDOM/script.js
@@ -0,0 +1,11 @@
+document.write('Hello, World!<br/>')
+document.write('<br/>' + window.document.charset)
+document.write('<br/>' + window.navigator.appName)
+document.write('<br/>' + window.document.URL)
+
+const p1 = document.getElementsByTagName('p')
+console.log(p1)
+const p2 = document.getElementsByTagName('p')
+console.log(p2.innerText)
+
+

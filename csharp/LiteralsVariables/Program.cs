a2616ec97528d63c06134131bfbbf3f8d43d830f
jardel-costa <jardel.costa@gmail.com>
2025-07-31 22:40:06 -0300

    "valores literais e variáveis"

---
+++ csharp/LiteralsVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿string name = "Bob";
+int message = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(message);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.WriteLine(" Celsius.");
+Console.ReadLine();

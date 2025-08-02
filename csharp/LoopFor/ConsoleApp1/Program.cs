8bd9e11212c0a3bebd322a3afb376b4cb43274b6
jardel-costa <jardel.costa@gmail.com>
2025-08-02 16:40:33 -0300

    Loop For

---
+++ csharp/LoopFor/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Digite um número inteiro para ver a sua tabuada: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+// inicialização: int i = 1 (comece a contar do 1)
+// condição: i<=10 (continue enquanto 'i' for menor ou igual a 10)
+// incremento: i++ (some 1 a 'i' a cada volta do loop)
+
+for (int i = 1; i <= 10; i++)
+{
+
+    Console.WriteLine(numero + "x" + i + "=" + (numero * i));
+
+}

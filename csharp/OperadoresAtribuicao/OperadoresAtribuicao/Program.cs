f7695f32ff502692bf73f864b4156a35f90edef5
jardel-costa <jardel.costa@gmail.com>
2025-08-05 19:31:17 -0300

    Operadores Atribuição

---
+++ csharp/OperadoresAtribuicao/OperadoresAtribuicao/Program.cs
@@ -0,0 +1,31 @@
+﻿
+//int z = int.Parse(null);
+int z = Convert.ToInt32(null);
+
+int a = 10;
+int b = 20;
+
+int c = a + b + z;
+
+Console.WriteLine(c);
+
+//c = c + 5;
+c += 5;
+
+Console.WriteLine(c);
+
+c -= 2;
+
+Console.WriteLine(c);
+
+c /= 3;
+
+Console.WriteLine(c);
+
+c *= 11;
+
+Console.WriteLine(c);
+
+c %= 7;
+
+Console.WriteLine(c);
